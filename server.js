b85c32a2326fb913f3140bb3cf478a1371e01ffa
volodymyr24082009 <24g_chvv@liceum.ztu.edu.ua>
2025-03-13 10:27:21 +0200

    1
    1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3006
+const port = process.env.PORT || 30019
 
 // Обслуговування статичних файлів з директорії 'public'
 app.use(express.static(path.join(__dirname, 'public')));
